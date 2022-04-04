88de8d070b0c78290350feeac5894a8ff910609b
unknown <omerme13@gmail.com>
2022-04-04 11:57:59 +0300

    feat: general error and loading spinner

--- client/src/components/staff/hooks/useStaff.ts
+++ client/src/components/staff/hooks/useStaff.ts
@@ -1,15 +1,9 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { filterByTreatment } from '../utils';
-
-// for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
 
 interface UseStaff {
   staff: Staff[];
@@ -17,12 +11,16 @@ interface UseStaff {
   setFilter: Dispatch<SetStateAction<string>>;
 }
 
+// for when we need a query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
+
 export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
-
-  // TODO: get data from server via useQuery
-  const staff = [];
+  const { data: staff = [] } = useQuery(queryKeys.staff, getStaff);
 
   return { staff, filter, setFilter };
 }
