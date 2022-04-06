50bf58da6349945735f5923a86ecf28dd2e4b199
unknown <omerme13@gmail.com>
2022-04-07 02:30:49 +0300

    feat(use appointments): get, prefetch and filter data

--- client/src/components/appointments/hooks/useAppointments.ts
+++ client/src/components/appointments/hooks/useAppointments.ts
@@ -1,6 +1,13 @@
 // @ts-nocheck
 import dayjs from 'dayjs';
-import { Dispatch, SetStateAction, useState } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -59,6 +66,11 @@ export function useAppointments(): UseAppointments {
   //   appointments that the logged-in user has reserved (in white)
   const { user } = useUser();
 
+  const handleSelect = useCallback(
+    (data: AppointmentDateMap) => getAvailableAppointments(data, user),
+    [user],
+  );
+
   /** ****************** END 2: filter appointments  ******************** */
   /** ****************** START 3: useQuery  ***************************** */
   // useQuery call for appointments for the current monthYear
@@ -70,8 +82,25 @@ export function useAppointments(): UseAppointments {
   //
   //    2. The getAppointments query function needs monthYear.year and
   //       monthYear.month
-  const appointments = {};
 
+  const { data: appointments = {} } = useQuery(
+    [queryKeys.appointments, monthYear.year, monthYear.month],
+    () => getAppointments(monthYear.year, monthYear.month),
+    {
+      select: showAll ? undefined : handleSelect,
+    },
+  );
+
+  const queryClient = useQueryClient();
+  const nextMonthYear = getNewMonthYear(monthYear, 1);
+  const { month: nextMonth, year: yearOfNextMonth } = nextMonthYear;
+
+  useEffect(() => {
+    queryClient.prefetchQuery(
+      [queryKeys.appointments, yearOfNextMonth, nextMonth],
+      () => getAppointments(yearOfNextMonth, nextMonth),
+    );
+  }, [queryClient, nextMonth, yearOfNextMonth]);
   /** ****************** END 3: useQuery  ******************************* */
 
   return { appointments, monthYear, updateMonthYear, showAll, setShowAll };
